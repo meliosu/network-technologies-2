5705f9294db391a14291b399115a135ef268d02c
Alexander <alex.atyurin@gmail.com>
2024-09-26 00:40:27 +0700

    removed main

--- lab4/src/main.rs
+++ lab4/src/main.rs
@@ -1,17 +1,5 @@
-use std::{
-    io,
-    net::{IpAddr, SocketAddr, ToSocketAddrs},
-};
-
-use lab4::net::Communicator;
+use std::io;
 
 fn main() -> io::Result<()> {
-    let mcast_addr = "239.192.0.4:9192".to_socket_addrs()?[0];
-    let comm = Communicator::new(mcast_addr)?;
-
-    loop {
-        todo!()
-    }
-
-    Ok(())
+    loop {}
 }
