be66a04d513116e3cb2522a95f356c5eff839110
Alexander <alex.atyurin@gmail.com>
2024-09-29 02:02:18 +0700

    added state to share between threads and ui<->model

--- lab4/src/state.rs
+++ lab4/src/state.rs
@@ -1,1 +1,29 @@
-pub struct State;
+use std::net::SocketAddr;
+
+use crate::{
+    logic::Game,
+    proto::{GameAnnouncement, GamePlayer, NodeRole},
+};
+
+pub struct State {
+    pub role: NodeRole,
+    pub game: Option<Game>,
+    pub players: Vec<Player>,
+    pub announcements: Vec<GameAnnouncement>,
+}
+
+pub struct Player {
+    pub player: GamePlayer,
+    pub addr: SocketAddr,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            role: NodeRole::Master,
+            game: None,
+            players: Vec::new(),
+            announcements: Vec::new(),
+        }
+    }
+}
