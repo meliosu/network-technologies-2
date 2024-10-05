396ccaedf518cfcbb8e41c7197cc3fb36b1be48e
Alexander <alex.atyurin@gmail.com>
2024-10-05 20:36:04 +0700

    added helpers for socket setup

---
+++ lab5/net.c
@@ -0,0 +1,84 @@
+#include <unistd.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include "net.h"
+
+int net_server(int port, int backlog) {
+    int err;
+
+    int sockfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, IPPROTO_TCP);
+    if (sockfd < 0) {
+        return -1;
+    }
+
+    int y = 1;
+    err = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &y, sizeof(y));
+    if (err) {
+        close(sockfd);
+        return -1;
+    }
+
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_addr.s_addr = htonl(INADDR_ANY),
+        .sin_port = htons(port),
+    };
+
+    err = bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    if (err) {
+        close(sockfd);
+        return -1;
+    }
+
+    err = listen(sockfd, backlog);
+    if (err) {
+        close(sockfd);
+        return -1;
+    }
+
+    return sockfd;
+}
+
+int net_dns() {
+    int err;
+
+    int sockfd = socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, IPPROTO_UDP);
+    if (sockfd < 0) {
+        return -1;
+    }
+
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_addr.s_addr = htonl(INADDR_ANY),
+        .sin_port = htons(0),
+    };
+
+    err = bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    if (err) {
+        close(sockfd);
+        return -1;
+    }
+
+    struct sockaddr_in dns_addr = {
+        .sin_family = AF_INET,
+        .sin_port = 53,
+    };
+
+    err = inet_pton(AF_INET, "8.8.8.8", &dns_addr.sin_addr);
+    if (err != 1) {
+        close(sockfd);
+        return -1;
+    }
+
+    err = connect(sockfd, (struct sockaddr *)&dns_addr, sizeof(dns_addr));
+    if (err) {
+        close(sockfd);
+        return -1;
+    }
+
+    return sockfd;
+}
---
+++ lab5/net.h
@@ -0,0 +1,7 @@
+#ifndef PROXY_NET_H
+#define PROXY_NET_H
+
+int net_server(int port, int backlog);
+int net_dns();
+
+#endif /* PROXY_NET_H */
