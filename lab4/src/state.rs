c1efa3f4b0e4f6f293809586714115296c59b629
Alexander <alex.atyurin@gmail.com>
2024-10-12 20:21:44 +0700

    add type for Announcement and helpers

--- lab4/src/state.rs
+++ lab4/src/state.rs
@@ -1,4 +1,7 @@
-use std::net::SocketAddr;
+use std::{
+    net::SocketAddr,
+    time::{Duration, Instant},
+};
 
 use crate::{
     config::Config,
@@ -12,12 +15,42 @@ pub struct State {
     pub role: NodeRole,
     pub game: Option<Game>,
     pub players: Vec<Player>,
-    pub announcements: Vec<(SocketAddr, GameAnnouncement)>,
+    pub announcements: Vec<Announcement>,
 }
 
 pub struct Player {
     pub player: GamePlayer,
     pub addr: SocketAddr,
+}
+
+pub struct Announcement {
+    pub timestamp: Instant,
+    pub addr: SocketAddr,
+    pub announce: GameAnnouncement,
+}
+
+impl Player {
+    pub fn new(addr: SocketAddr, player: GamePlayer) -> Self {
+        Self { player, addr }
+    }
+}
+
+impl Announcement {
+    pub fn new(addr: SocketAddr, announce: GameAnnouncement) -> Self {
+        Self {
+            addr,
+            announce,
+            timestamp: Instant::now(),
+        }
+    }
+
+    pub fn refresh(&mut self) {
+        self.timestamp = Instant::now();
+    }
+
+    pub fn elapsed(&self) -> Duration {
+        self.timestamp.elapsed()
+    }
 }
 
 impl State {
